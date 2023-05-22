8149c781cedcdc375a887f14de323a7efd8cb2e5
aswinkprakasan <aswinkuttanchery@gmail.com>
2023-05-22 16:40:06 +0530

    login and registration with firebase

---
+++ app/src/main/java/com/example/koutapp/DashboardActivity.java
@@ -0,0 +1,48 @@
+package com.example.koutapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    TextView details;
+    Button button;
+    FirebaseAuth mAuth;
+    FirebaseUser user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        mAuth = FirebaseAuth.getInstance();
+        details = findViewById(R.id.details);
+        button = findViewById(R.id.logout);
+        user = mAuth.getCurrentUser();
+
+        if (user == null){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        }
+        else {
+            details.setText(user.getEmail());
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/koutapp/SignupTabFragment.java
@@ -0,0 +1,88 @@
+package com.example.koutapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class SignupTabFragment extends Fragment {
+    EditText email, password, repassword;
+    ProgressBar progressBar;
+    Button button;
+    FirebaseAuth mAuth;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getActivity(), DashboardActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_signup_tab, container, false);
+
+        email = view.findViewById(R.id.signup_email);
+        password = view.findViewById(R.id.signup_password);
+        repassword = view.findViewById(R.id.signup_confirm);
+        progressBar = view.findViewById(R.id.progressBar);
+        button = view.findViewById(R.id.signup_button);
+        mAuth = FirebaseAuth.getInstance();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                String mailid, pass, repass;
+                mailid = String.valueOf(email.getText());
+                pass = String.valueOf(password.getText());
+                repass = String.valueOf(repassword.getText());
+
+                if (TextUtils.isEmpty(mailid) || TextUtils.isEmpty(pass) || TextUtils.isEmpty(repass)){
+                    Toast.makeText(getActivity(), "don't leave the blank empty", Toast.LENGTH_SHORT).show();
+                }
+
+                mAuth.createUserWithEmailAndPassword(mailid, pass)
+                        .addOnCompleteListener(requireActivity(), new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(requireContext(), "Account created",
+                                            Toast.LENGTH_SHORT).show();
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(requireContext(), "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+        return view;
+    }
+}
